f2566c4a273b6755de56cc6271d368285e0375cc
djccnt15 <djccnt15@gmail.com>
2024-01-19 23:59:55 +0900

    update UserInfoEntity in user_entity

--- src/db/entity/user_entity.py
+++ src/db/entity/user_entity.py
@@ -1,5 +1,5 @@
 from sqlalchemy.schema import Column
-from sqlalchemy.types import String
+from sqlalchemy.types import LargeBinary, String
 
 from .base_entity import BaseEntity
 
@@ -7,8 +7,33 @@ from .base_entity import BaseEntity
 class UserInfoEntity(BaseEntity):
     __tablename__ = "user_list"
 
-    user_name = Column(type_=String(50), name="user_name", nullable=False)
-    enc_session_key = Column(type_=String(1000), name="enc_session_key", nullable=False)
-    nonce = Column(type_=String(100), name="nonce", nullable=False)
-    tag = Column(type_=String(100), name="tag", nullable=False)
-    ciphertext = Column(type_=String(100), name="ciphertext", nullable=False)
+    user_name = Column(
+        type_=String(50),
+        name="user_name",
+        nullable=False,
+    )
+    messenger = Column(
+        type_=String(20),
+        name="messenger",
+        nullable=False,
+    )
+    enc_session_key = Column(
+        type_=LargeBinary(512),
+        name="enc_session_key",
+        nullable=False,
+    )
+    nonce = Column(
+        type_=LargeBinary(32),
+        name="nonce",
+        nullable=False,
+    )
+    tag = Column(
+        type_=LargeBinary(32),
+        name="tag",
+        nullable=False,
+    )
+    ciphertext = Column(
+        type_=LargeBinary(512),
+        name="ciphertext",
+        nullable=False,
+    )
