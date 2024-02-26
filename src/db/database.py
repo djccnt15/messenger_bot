cab4ef1b7565d091499386e1ba28180d715ed9f1
Jay.D <djccnt15@gmail.com>
2024-02-26 10:59:16 +0900

    update db configuration

--- src/db/database.py
+++ src/db/database.py
@@ -7,21 +7,9 @@ from src.config import configs
 
 db_config = configs.config.db
 
-SQLALCHEMY_DATABASE_URL = URL.create(
-    drivername=db_config.drivername,
-    username=db_config.username if db_config.username else None,
-    password=db_config.password if db_config.password else None,
-    host=db_config.host if db_config.host else None,
-    port=int(db_config.port) if db_config.port else None,
-    database=db_config.database,
-)
+SQLALCHEMY_DATABASE_URL = URL.create(**db_config.url)
 
-engine = create_engine(
-    url=SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True,
-    pool_recycle=3600,
-    echo=db_config.echo,
-)
+engine = create_engine(url=SQLALCHEMY_DATABASE_URL, **db_config.engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
